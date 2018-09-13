30c126167ec0716aa019aaff18ed9206a09dc2a2
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-09-13 22:41:03 +0200

    - 3 methods are created

--- LibraryManagement/Data/Interfaces/IBookRepository.cs
+++ LibraryManagement/Data/Interfaces/IBookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagement.Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,8 @@ namespace LibraryManagement.Data.Interfaces
 {
     interface IBookRepository
     {
+        IEnumerable<Book> GetAllWithAuthor();
+        IEnumerable<Book> FindWithAuthor(Func<Book, bool> predicate);
+        IEnumerable<Book> FindWithAuthorAndBorrower(Func<Book, bool> predicate);
     }
 }
