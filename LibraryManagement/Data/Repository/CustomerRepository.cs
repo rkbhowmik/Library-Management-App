f5f2dc8bb9e4bb429eab9a4602858fadcb3f9f49
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-09-14 00:03:44 +0200

    Customer Interface implemented

--- LibraryManagement/Data/Repository/CustomerRepository.cs
+++ LibraryManagement/Data/Repository/CustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagement.Data.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace LibraryManagement.Data.Repository
 {
     public class CustomerRepository : ICustomerRepository
     {
+        // constructor
+
+        protected readonly LibraryDbContext _context;
+
+        public CustomerRepository (LibraryDbContext context)
+        {
+            _context = context;
+        }
+
     }
 }
