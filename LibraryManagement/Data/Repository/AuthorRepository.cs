b0fe89c1b4b371d866f9fb6c70db27c3c22fa242
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-09-13 23:48:54 +0200

    - Author interface is implemented

---
+++ LibraryManagement/Data/Repository/AuthorRepository.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagement.Data.Interfaces;
+using LibraryManagement.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        protected readonly LibraryDbContext _context;
+
+        public AuthorRepository (LibraryDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Author> GetAllWithBooks()
+        {
+            // Include method will return all the entities from the Author class
+            return _context.Authors.Include(a => a.Books);
+        }
+
+        // First it will search Author by its id and then returns associated books
+        // and load it
+        public Author GetWithBooks(int id)
+        {
+            return _context.Authors.Where(a => a.AuthorId == id)
+                .Include(a => a.Books)
+                .FirstOrDefault();
+        }
+    }
+}
