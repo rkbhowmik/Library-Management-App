cbec93acfab345b7191dc487ffecb33f1eb4abd2
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-09-13 23:20:37 +0200

    -LibraryDbContext class created -Interface implemented

---
+++ LibraryManagement/Data/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Data.Repository
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
+}
